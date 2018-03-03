0805c777d453067d412d77c1bc4dc0e6c749e05a
Chris Troutner <chris.troutner@gmail.com>
2018-03-03 13:08:54 -0800

    Fixing keystone.js file

--- keystone/keystone.js
+++ keystone/keystone.js
@@ -33,7 +33,7 @@ keystone.init({
 	'session': true,
 	'auth': true,
 	'user model': 'User',
-	'port': 3002,
+	'port': 3000,
   'signin redirect': '/usersettings',
   'signout redirect': '/',
 	'mongo': 'mongodb://172.17.0.1:3502/map-tracks'
